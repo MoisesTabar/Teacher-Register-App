1ad96616cee30e838bba4a053fbb7fbe66edf8c2
MoisesTabar <moisestabar.013@gmail.com>
2020-09-18 16:45:11 -0400

    Source files edited add upload in localStorage

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 //added onload animation
 const div = document.querySelector('.mainDiv');
+const secondDiv = document.querySelector('.secondDiv');
 const form = document.querySelector('#form');
 window.addEventListener('DOMContentLoaded', () => {
     div.style.opacity = 0;
+    secondDiv.style.opacity = 0;
     form.style.opacity = 0;
     setTimeout(() =>{
         div.style.opacity = 1;
@@ -34,7 +36,6 @@ const urlSchools = 'http://localhost:3000/schools';
 const fillSchools = async () => {
     const request = await fetch(urlSchools);
     const response = await request.json();
-    const {name} = response;
 
     for(elements in response){
        selectSchools.options[selectSchools.options.length] = new Option(JSON.stringify(response[elements].name));
@@ -52,22 +53,82 @@ const fillComps = async () => {
 }
 fillSchools();
 fillComps();
-
+//arrays of the table
+let columns = [];
+let column = [];
 //input the id so it can match the data from an api and display it later in a table 
-const inputID = document.querySelector('#id').value;
-const url = `https://api.adamix.net/apec/cedula/${inputID}`;
-
+const inputID = document.querySelector('#id');
 const queryID = async () => {
+    let url = `https://api.adamix.net/apec/cedula/${inputID.value}`;
     const request = await fetch(url);
     const response = await request.json();
-    console.log(response);
+    //fields specified
+    const {Nombres, Apellido1, FechaNacimiento, IdSexo ,foto} = response;
+    column.push(Nombres)
+    column.push(Apellido1)
+    column.push(FechaNacimiento)
+    column.push(IdSexo)
+    column.push(foto)
 }
-queryID();
 
 //validating the form
 const mail = document.querySelector('#mail');
 const image = document.querySelector('#image');
 const submit = document.querySelector('#submit')
-form.addEventListener('submit', (e) => {
+
+const formValidation = () => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+        if(inputID.value === ''||mail.value === '' || selectSchools.selectedIndex == 0 || selectComp.selectedIndex == 0|| image.value == ''){
+            console.log('empty fields')
+        }
+        else{
+            const fill = () => {
+                column.push(inputID.value)
+                column.push(mail.value)
+                column.push(selectSchools.options[selectSchools.options.selectedIndex].text)
+                column.push(selectComp.options[selectComp.options.selectedIndex].text)
+                column.push(image.value);
+                
+                columns.push(column);
+                const data = JSON.stringify(columns);
+                localStorage.setItem('data', data);
+            }
+            fill()
+            alert('Registration completed!')
+        }
+    }); 
+
+    const querySubmit = document.querySelector('#querySubmit');
+
+    querySubmit.addEventListener('click', (e) => {
     e.preventDefault();
-})
+    const showData = () => {
+        const tBody = document.querySelector('#tbodyData');
+        tBody.innerHTML = '';
+            setTimeout(() => {
+                secondDiv.style.opacity = 1;
+            }, 0500);
+
+            for(let x = 0; x < columns.length; x++){
+                let tr = document.createElement('tr');
+                column = columns[x];
+
+                for(let y = 0; y < column.length; y++){
+                    let data = column[y];
+                    let td = document.createElement('td');
+                    td.innerHTML = data;
+                    tr.appendChild(td)
+                }
+                tBody.appendChild(tr);
+            }
+        }
+        showData();
+        const data = localStorage.getItem('data');
+        if(data != null){
+            columns = JSON.parse(data);
+            showData();
+        }
+    });
+}
+formValidation();
